3fc4aa22198dfe1f7e0a7d0866942a88db955de9
Ortal Yadayev <ortalll20111@gmail.com>
2022-07-01 08:22:31 +0300

    fixed

--- app.py
+++ app.py
@@ -1,27 +1,57 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 import json
 
-# bp = Blueprint('burritos', __name__, template_folder='templates')
-preffix = '/api'
+preffix = '/api/v1'
 
 app = Flask(__name__)
-# app.register_blueprint(bp, url_prefix='/api')
 
 @app.route('/')
 def hello():
     return "Server is working!"
 
-from flask import request
-@app.route(preffix + '/v1/attack')
-def getAttack():
+@app.route(preffix + '/attack')
+def aws():
     startTime = datetime.now()
     id = request.args.get('vm_id', default = '*', type = str)
+    attacks = []
     
-    with open('./data/input-0.json', 'r') as jsonFile:
-        data = json.load(jsonFile)
-        jsonFile.close()
+    for i in [0,1,2,3]:
+        file = './data/input-%d.json' %(i) 
+        flag = False
         
+        with open(file, 'r') as jsonFile:
+            data = json.load(jsonFile)
+            jsonFile.close()
+            
+        for vm in data['vms']:
+            if(vm['vm_id'] == id): 
+                attacks = getAttack(data, id)
+                flag = True
+                break
+        
+        if(flag):
+            endTime = datetime.now()
+            dt = endTime - startTime
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            
+            if 'request_count' not in data:
+                data.update({'request_count': 0})
+                data.update({'average_request_time' : 0})
+            
+            avg = (data['request_count'] * data['average_request_time'] + ms) / (data['request_count'] + 1)
+            data['request_count'] += 1
+            data['average_request_time'] = avg
+
+            with open(file, 'w') as newJsonFile:
+                json.dump(data, newJsonFile)
+                newJsonFile.close()
+            
+            break
+    
+    return jsonify(attacks)
+
+def getAttack(data, id):
     tags = []
     sources = []
     canAttacks = []
@@ -29,56 +59,38 @@ def getAttack():
     for vm in data['vms']:
         if(vm['vm_id'] == id): 
             tags = vm['tags']
-            
+       
     for rule in data['fw_rules']:
-        for tag in tags:
-            if(tag == rule['dest_tag']):
-                sources.append(rule['source_tag'])
+        if rule['dest_tag'] in tags:
+            sources.append(rule['source_tag'])
            
     for vm in data['vms']:
         for source in sources:
-            for tag in vm['tags']:
-                if(source == tag and vm['vm_id'] != id):
-                    canAttacks.append(vm['vm_id'])
-                    break
-                    
-    endTime = datetime.now()
-    dt = endTime - startTime
-    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            if source in vm['tags'] and vm['vm_id'] != id:
+                canAttacks.append(vm['vm_id'])
+                break
+              
+    return canAttacks
     
-    if 'request_count' not in data:
-        data.update({'request_count': 0})
-        data.update({'average_request_time' : 0})
-    
-    avg = (data['request_count'] * data['average_request_time'] + ms) / (data['request_count'] + 1)
-    data['request_count'] += 1
-    data['average_request_time'] = avg
-    
-    print(ms)
-    print(data)
-    
-    with open('./data/input-0.json', 'w') as newJsonFile:
-        json.dump(data, newJsonFile)
-        newJsonFile.close()
-      
-    print(canAttacks)  
-    # return '
-    return jsonify(canAttacks)
-    
-@app.route(preffix + '/v1/stats')
+@app.route(preffix + '/stats')
 def stats():
-    with open('./data/input-0.json', 'r') as jsonFile:
-        data = json.load(jsonFile)
-        jsonFile.close()
-    
-    if 'request_count' not in data:
-        data.update({'request_count': 0})
-        data.update({'average_request_time' : 0})
+    result = []
+    for i in [0,1,2,3]:
+        file = './data/input-%d.json' %(i) 
+        flag = False
         
-    result = { 
-        'vm_count': len(data['vms']),
-        'request_count': data['request_count'], 
-        'average_request_time': data['average_request_time']
-    }
+        with open(file, 'r') as jsonFile:
+            data = json.load(jsonFile)
+            jsonFile.close()
+            
+        if 'request_count' not in data:
+            data.update({'request_count': 0})
+            data.update({'average_request_time' : 0})
+            
+        result.append({ 
+            'vm_count': len(data['vms']),
+            'request_count': data['request_count'], 
+            'average_request_time': data['average_request_time']
+        })
     
-    return result
+    return jsonify(result)
